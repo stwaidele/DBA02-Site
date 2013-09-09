a2e73a161e121edfa63fd812b5f64347a307adc8
Stefan Waidele <Stefan@Waidele.info>
2013-09-09 23:57:52 +0200

    Löschknöpfe funktionieren

--- pages/neuefrage.php
+++ pages/neuefrage.php
@@ -15,17 +15,22 @@
 			<button type="button" id="newanswer" class="btn btn-success btn-xs">Weitere Antwort eingeben...</button>
 		</div>	
 		<script>
+		
 		$("#newanswer").click(function() 
 		{
 			var nq = $("<div class='form-group'>\
 			<label for='antwort3'>Weitere Antwortmöglichkeit</label>\
 			<div class='input-group'><input type='text' class='form-control'  placeholder='Hier muss noch eine eindeutige HTML-ID generiert werden.'>\
-			<span class='input-group-btn'><button type='button' class='btn btn-danger' aria-hidden='true'>&times;</button></span>\
+			<span class='input-group-btn'><button type='button' class='delanswer btn btn-danger' aria-hidden='true'>&times;</button></span>\
 			</div></div>");
 			nq.hide();
 			nq.appendTo("#answers");
 			nq.slideDown();	
-		})
+			$(".delanswer").click(function(){
+				$(this).parent().parent().parent().slideUp();
+				$(this).parent().parent().parent().remove();
+			});
+		});
 		</script>
 		<button type="submit" class="btn btn-default">Frage & Antworten speichern</button>
 	</form>
