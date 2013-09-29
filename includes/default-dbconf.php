a870726eb0022225bfe4907683dd9ca65f3d4f8a
Stefan Waidele <Stefan@Waidele.info>
2013-09-29 13:40:12 +0200

    dbconf.php -> .gitignore

---
+++ includes/default-dbconf.php
@@ -0,0 +1,8 @@
+<?php 
+// Die folgenden Variablen müssen für den
+// jeweiligen Datenbandserver angepasst werden.
+// Anschließend diese Datei in dbconf.php umbenennen.
+	$DBA02_host = "127.0.0.1";
+	$DBA02_user = "dba02";
+	$DBA02_pass = "";
+	?>	
