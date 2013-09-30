c320fed18d96341ce65f6a6bab386c51399f5de1
Stefan Waidele <Stefan@Waidele.info>
2013-09-30 20:17:45 +0200

    YFs Seiten
    wg. Git-Problemen von mir...

--- pages/auswertung.php
+++ pages/auswertung.php
@@ -1,52 +1,62 @@
-<div ID="FID-42">
+<?php
+$fid =$_POST['daten'];
+
+//Verbindung zur DB
+$connection = @mysql_connect($DBA02_host, $DBA02_user, $DBA02_pass);
+if ($connection == FALSE) {
+	echo "Bitte entschuldigen Sie, es ist ein technischer Fehler aufgetreten. Bitte wenden Sie sich an den Support";
+	exit();
+}
+
+mysql_select_db("dba02");
+
+$abfrage= mysql_query("Select txt from frage where fid=".$fid);
+$frage = mysql_fetch_row($abfrage);
+
+$abfrantw = mysql_query ("Select txt, aid from antwort where fid =".$fid);
+$anzahlant = mysql_num_rows($abfrantw);
+
+for ($i=1; $i<=$anzahlant; $i++)
+{
+	$chckbname = "AID".$i;
+	if (isset ($_POST[$chckbname]))
+	{
+		$chckbwert = $_POST[$chckbname];
+		mysql_query("INSERT INTO geantwortet(aid)VALUES('$chckbwert')");
+	}
+}
+
+$abfrggesamtant = mysql_query("SELECT * FROM antwort ,geantwortet WHERE antwort.fid =".$fid." AND antwort.aid = geantwortet.aid");
+
+$gesamtsumme = mysql_num_rows($abfrggesamtant);
+?>
+
+<div>
 	<h2>Auswertung</h2>
 	<p>Vielen Dank für Ihre Antwort.</p>
-	<h3>Wie lautet die Antowrt auf die große Frage des Lebens, des Universums, und einfach allem?</h3>
-			<h4>FID = "<?php echo $_GET["fid"]; ?>"</h4>
-	<div ID="AID-42">
-		<p>Zweiundvierzig!</p>
-		<div class="progress progress-striped">
-			<div class="progress-bar" role="progressbar" 
-			aria-valuenow="60" 
-			aria-valuemin="0" aria-valuemax="100" 
-			style="width: 60%;">
-			<span class="sr-only">60%</span>
-		</div>
-	</div>
-</div>
-	<div ID="AID-17">	
-		<p>WTF???</p>
-		<div class="progress progress-striped">
-			<div class="progress-bar" role="progressbar" 
-			aria-valuenow="10" 
-			aria-valuemin="0" aria-valuemax="100" 
-			style="width: 10%;">
-			<span class="sr-only">10%</span>
-		</div>
-	</div>
-</div>
-	<div ID="AID-99">
-		<p>Hasta la Vista, Baby.</p>
-		<div class="progress progress-striped">
-			<div class="progress-bar" role="progressbar" 
-			aria-valuenow="25" 
-			aria-valuemin="0" aria-valuemax="100" 
-			style="width: 25%;">
-			<span class="sr-only">25%</span>
-		</div>
-	</div>	
-</div>
-	<div ID="AID-23">
-		<p>Basically...RUN!</p>
-		<div class="progress progress-striped">
-			<div class="progress-bar" role="progressbar" 
-			aria-valuenow="30" 
-			aria-valuemin="0" aria-valuemax="100" 
-			style="width: 30%;">
-			<span class="sr-only">30%</span>
-		</div>
-	</div>
-</div>
-</div>
+	<h3><?php echo $frage[0] ?></h3>
+<?php
+	for ($i = 1; $i <= $anzahlant; $i++) {
+			$antwortm = mysql_fetch_row($abfrantw, MYSQL_BOTH);
+			echo $antwortm[0];
+			$aid = $antwortm[1];
+			$abfrgeinzlantw = mysql_query("SELECT * FROM antwort, geantwortet WHERE antwort.fid =".$fid." and antwort.aid = geantwortet.aid and geantwortet.aid=".$aid);
+			$gesamtantm = mysql_num_rows($abfrgeinzlantw);
+			$rechnung = (100/$gesamtsumme)*$gesamtantm;
+			$rechnung = number_format($rechnung, 2,",",".");
+			$prozent = $rechnung."%";
+			$prozent= strval($prozent);
+			printf("<div class=\"progress progress-striped\">");
+			printf("<div class=\"progress-bar\" role=\"progressbar\" 
+			aria-valuenow=\"10\"
+			aria-valuemin=\"0\" aria-valuemax=\"100\"
+			style=\"width: %s\">",$prozent);
+			printf("<span class=\"meter\">%s</span>",$prozent);
+		printf("</div>");
+	printf("</div>");
+}
 
+?>
+</div>
 <a class="btn btn-default" href="/frage/2001">Nächste Frage</a>
+
--- pages/frage.php
+++ pages/frage.php
@@ -2,12 +2,11 @@
 
 //Verbindung zur DB
 $connection = @mysql_connect($DBA02_host, $DBA02_user, $DBA02_pass);
-if ($connection == FALSE)
-{
-echo "Bitte entschuldigen Sie, es ist ein technischer Fehler aufgetreten. Bitte wenden Sie sich an den Support";
-exit();
-}
 
+if ($connection == FALSE) {
+	echo "Bitte entschuldigen Sie, es ist ein technischer Fehler aufgetreten. Bitte wenden Sie sich an den Support";
+	exit();
+}
 
 mysql_select_db("dba02");
 
@@ -18,42 +17,36 @@ $zufall = mt_rand(1, $anzahl);
 $frage = mysql_query ("Select txt from frage where fid=".$zufall);
 $auswahl = mysql_fetch_row($frage);
 
-$abfrantw = mysql_query ("Select txt from antwort where fid =".$zufall);
+$abfrantw = mysql_query ("Select aid, txt from antwort where fid =".$zufall);
 $anzahlant = mysql_num_rows($abfrantw);
 
 $daten = $zufall;
-
-
 ?>
 
 <div ID="frage">
 	<h2>Bitte beantworten Sie folgende Frage:</h3>
-
 		<h3><?php echo $auswahl[0] ?></h3>
-		
-		<form role="form" action="/auswertung/<?php echo $zufall ?>" method ="post">
-
+		<form role="form" action="/dba02/index.php? show=auswertung" method ="post">
 		<input type="hidden" name="daten" value="<?php echo $daten; ?>" />
-
 <?php
 	for ($i = 1; $i <= $anzahlant; $i++) {
- 
-	$antwortm = mysql_fetch_row($abfrantw, MYSQL_BOTH);
-
+			$antwortm = mysql_fetch_array($abfrantw, MYSQL_BOTH);
+			//$antwortm = mysql_fetch_row($abfrantw, MYSQL_BOTH);
 			printf("<div class=\"checkbox\">");
 				printf("<label>");
-				printf("<input type=\"checkbox\" name=\"AID-%d\">", $i);
-				echo $antwortm[0];
+				printf("<input type=\"checkbox\" name=\"AID%d\" value=\"%s\">", $i, $antwortm[0]);
+				echo $antwortm[1];
 			printf("</label>");
 			printf("</div>");
-}
-
+	}
 ?>
-			 
+
+		 
+
 			<button type="submit" class="btn btn-default">Antworten & sehen, was andere geantwortet haben</button>
 		</form>
 	</div>
-<?
+<?php
 mysql_close($connection);
 ?>
 
