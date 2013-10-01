2aa36a3fe89d4920524dc65e33ce40d22b2f3f3b
Stefan Waidele <Stefan@Waidele.info>
2013-10-01 12:01:59 +0200

    Aufräumen und Kommentieren

--- pages/speichern.php
+++ pages/speichern.php
@@ -7,7 +7,10 @@ if ($auth_angemeldet==FALSE) {
 	<p><a href="/anmeldung">Zur Anmeldeseite...</a></p>
 	<?php
 } else {
+	// $fragetext ist ein String
 	$fragetext = $_REQUEST["frage"];
+	// $antworten ist ein Array aus Strings (wegen gleicher "name" im HTML-Formular )
+	// http://stackoverflow.com/a/1942432
 	$antworten = $_REQUEST["awm"];
 	
 //	$sql[0] = "insert into "
@@ -16,45 +19,7 @@ if ($auth_angemeldet==FALSE) {
 		print $antwort;
 	}
 	
+	// Nächste Frage...
+	include($_SERVER['DOCUMENT_ROOT'].'/pages/neuefrage.php'); 
+}
 ?>
-	<div ID="NewQuestion">
-
-
-		<h3>Bitte geben Sie Ihre neue Frage ein:</h3>
-		<form role="form" action="/speichern" method="post">
-			<div class="form-group">
-				<label for="frage">Fragetext</label>
-				<input type="text" class="form-control" id="frage" name="frage" placeholder="Wie lautet Ihre neue Frage?">
-			</div>
-			<div id="answers">
-				<div class="form-group">
-					<label for="antwort1">1. Antwortmöglichkeit</label>
-					<input type="text" class="form-control" name='awm[]'  placeholder="Antwort eingeben...">
-				</div>
-			</div>
-			<div class="form-group">
-				<button type="button" id="newanswer" class="btn btn-success btn-xs">Antwort eingeben...</button>
-			</div>	
-			<script>
-		
-			$("#newanswer").click(function() 
-			{
-				var na = $("<div class='form-group'>\
-				<label for='antwort'>Weitere Antwortmöglichkeit</label>\
-				<div class='input-group'><input type='text' class='form-control' name='awm[]'  placeholder='Antwort eingeben...'>\
-				<span class='input-group-btn'><button type='button' class='delanswer btn btn-danger' aria-hidden='true'>&times;</button></span>\
-				</div></div>");
-				na.hide();
-				na.appendTo("#answers");
-				na.slideDown();	
-				$(".delanswer").click(function(){
-					$(this).parent().parent().parent().slideUp("normal",
-					function() {$(this).remove();}
-				);
-			});
-		});
-		</script>
-		<button type="submit" class="btn btn-default">Frage & Antworten speichern</button>
-	</form>
-</div>
-<?php } ?>
